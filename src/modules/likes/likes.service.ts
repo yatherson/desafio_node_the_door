bce6e7ccddd419bc30c6b6634d534f9e10c81a1e
Yatherson Lucas <yathersonlucas@hotmail.com>
2026-04-23 12:44:26 -0300

    fix: dependencia sem uso

--- src/modules/likes/likes.service.ts
+++ src/modules/likes/likes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
 import { PostsRepository } from '../posts/posts.repository';
